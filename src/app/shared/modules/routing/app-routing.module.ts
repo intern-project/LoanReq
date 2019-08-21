6b557a686f14b76f9c75ea66d31536d75932f083
ndhpeiris <42838688+ndhpeiris@users.noreply.github.com>
2019-08-21 20:46:07 +0530

    mearge conflict

--- src/app/shared/modules/routing/app-routing.module.ts
+++ src/app/shared/modules/routing/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from 'src/app/components/auth/login/login.component';
 import { PendingRequestsComponent } from 'src/app/components/admin/pending-requests/pending-requests.component';
 import { ReviewedRequestsComponent } from 'src/app/components/admin/reviewed-requests/reviewed-requests.component';
+import {MakeRequestComponent} from 'src/app/components/officer/make-request/make-request.component';
 
 
 const routes: Routes = [
   { path: 'pending', component: PendingRequestsComponent },
   { path: 'reviewed', component: ReviewedRequestsComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'makeReq', component: MakeRequestComponent },
 ];
 
 @NgModule({
