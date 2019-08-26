5abd21d32f32a2112aa3d069e3a5e6e34b514012
ndhpeiris <42838688+ndhpeiris@users.noreply.github.com>
2019-08-26 17:15:48 +0530

    comit makeReq 4

--- src/app/components/officer/make-request/make-request.component.ts
+++ src/app/components/officer/make-request/make-request.component.ts
@@ -6,7 +6,10 @@ import { FormGroup, FormBuilder, Validators, } from '@angular/forms';
 import { SelectItem } from 'primeng/api';
 import { LoanType } from 'src/app/shared/classes/loan-type';
 import {RequestService} from 'src/app/shared/services/request/request.service';
-
+import { CommonModule } from '@angular/common';  
+import { BrowserModule } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -16,46 +19,77 @@ import {RequestService} from 'src/app/shared/services/request/request.service';
 })
 export class MakeRequestComponent implements OnInit {
 
-  loanTypeForm: FormGroup;
-  loanTypes: LoanType[];
-  loanType: LoanType[];
-  cloned: { [s: string]: LoanType; } = {};
+  public loanList: any[] = [];
+  addLoan:boolean = false;
+  reqMakeForm: FormGroup;
   uploadedFiles: any[] = [];
 
-  public step1: boolean = true;
-  public step2: boolean = false;
-  public step3: boolean = false;
+  
 
+  private _jsonURL = '../../../../assets/requests.json';
+
+  requests: Request[];
 
   constructor(
     public fb: FormBuilder,
-    private messageService: RequestService
-  ) {
+    private messageService: RequestService,
+    private http: HttpClient,
+    private router: Router
+  ) {this.getJSON().subscribe(data => {
+    console.log(data.request);
+    this.requests = data.request;
+  });
+}
 
-  }
+public getJSON(): Observable<any> {
+  return this.http.get(this._jsonURL);
+}
+
+  
 
   ngOnInit() {
-    this.initForm();
+    this.ReqForm();
   }
 
-  initForm() {
-    this.loanTypeForm = this.fb.group({
-      id: [''],
-      type: ['', Validators.required],
-      rate: ['', Validators.required]
+  ReqForm() {
+    this.reqMakeForm = this.fb.group({
+      rid: [''],
+      name: ['', Validators.required],
+      address: ['', Validators.required],
+      age: ['', Validators.required],
+      job: ['', Validators.required],
+      contact: ['', Validators.required],
+      nic: ['', Validators.required],
+      ammount: ['', Validators.required],
+      duration: ['', Validators.required],
+      reason: ['', Validators.required],
+      doc: ['0', Validators.required],
+      pending: ['1', Validators.required],
+      accepted: ['0', Validators.required],
+      declined: ['0', Validators.required]
     });
   }
 
   onSubmit() {
-    console.log(this.loanTypeForm.value);
-    const data = this.loanTypeForm.value;
-    this.loanTypes.push(data);
-
-    this.initForm();
+    console.log(this.reqMakeForm.value);
+    const data = this.reqMakeForm.value;
+    this.loanList.push(data);
+    console.log(this.loanList);
   }
-  onCancel() { }
 
-  
+
+  onCancel() { 
+    this.addLoan = false;
+  }
+
+  onUpload(event) {
+    for(let file of event.files) {
+        this.uploadedFiles.push(file);
+    }
+    
+    this.messageService.add({severity: 'info', summary: 'File Uploaded', detail: ''});
+    console.log(this.uploadedFiles);
+}
 
  
 }
