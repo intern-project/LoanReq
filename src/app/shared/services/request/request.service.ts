63e88bc0f9ecb05db348aff45e3460834cc2bf83
udara-bandara <jmusbandara16@gmail.com>
2019-09-02 11:41:32 +0530

    some changes

--- src/app/shared/services/request/request.service.ts
+++ src/app/shared/services/request/request.service.ts
@@ -2,29 +2,30 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Request } from '../../classes/request';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class RequestService {
-  private _jsonURL = 'https://localhost:5001/api/request';
+   jsonURL = 'https://localhost:5001/api/Request';
   requests: Request[];
 
-  private responseData:any;
-  private loanData:any;
+  private responseData: any;
+  private loanData: any;
 
-  constructor(private http: HttpClient, public router:Router) { }
+  constructor(private http: HttpClient, public router: Router) { }
 
-  //Get All
-  getJSON(){
+  // Get All
+  getJSON() {
 
-    let promise = new Promise((resolve, reject) => {
-      this.http.get(this._jsonURL)
+    const promise = new Promise((resolve, reject) => {
+      this.http.get(this.jsonURL)
         .toPromise()
         .then(
           res => { // Success
-            this.requests = res as Request[]
+            this.requests = res as Request[];
             console.log(this.requests);
             localStorage.setItem('req', JSON.stringify(this.requests));
             resolve();
@@ -39,17 +40,17 @@ export class RequestService {
     header.set('content-Type', 'application/json');
     const options = { headers: header };
     const id = request.rid;
-    const url = `${this._jsonURL}/${id}`;
+    const url = `${this.jsonURL}/${id}`;
     this.http.put<Request>(url, request, options).
-      subscribe(val => {console.log('done')
-                        this.router.navigate(['/admin/reviewed'])
+      subscribe(val => {console.log('done');
+                        this.router.navigate(['/admin/reviewed']);
                         },
-        response => {console.log("Error", response)})
+        response => {console.log('Error', response);});
   }
 
-  
 
-  add({severity: string, summary: sum, detail: any}){
+
+  add({severity: string, summary: sum, detail: any}) {
 
 
   }
@@ -71,16 +72,17 @@ export class RequestService {
 
   // }
 
-  addLoan(data:any){
-    console.log("came to make-request service");
+  addLoan(data: Request): Observable<Request> {
+    const header = new HttpHeaders();
+    header.set('content-Type', 'application/json');
+    const options = { headers: header };
+    console.log('came to make-request service');
     console.log(data);
-     this.loanData= {data: data};
-     console.log(this.loanData);
+    this.loanData = {data};
+    console.log(this.loanData);
+    // console.log(this.http.post<Request>(this.jsonURL, data));
 
-    this.http.post<{token: string}>(this._jsonURL,this.loanData).subscribe((response)=>{
-
-    });
-
+    return this.http.post<Request>(this.jsonURL, data, options);
 
   }
 
